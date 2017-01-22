ef0d0a7337577f83e872e6889cf6d0e5daeaf2ec
Izhar Firdaus <kagesenshi.87@gmail.com>
2017-01-22 13:27:38 +0800

    added split_by utilization

--- sqoop_tester.py
+++ sqoop_tester.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import psutil
 import time
 import os, socket
@@ -7,6 +8,7 @@ from pprint import pprint
 import sys
 import traceback
 import csv
+from datetime import datetime
 
 def send_nifi(url, data):
     print data
@@ -22,16 +24,23 @@ def start(sessionid, sources, nifi_url):
         "sqoop-import --hive-import --hive-overwrite --create-hive-table "
         "--connect jdbc:oracle:thin:@//%(host)s:%(port)s/%(tns)s --username "
         "%(username)s --password %(password)s --table %(schema)s.%(table)s -m "
-        "%(mapper)s"
+        "%(mapper)s --direct "
     )
 
     data = []
     for row in csv.DictReader(open(sources), delimiter=','):
         data.append(row)
 
-    for row in data:
+    source_count = len(data) - 1
+    for idx, row in enumerate(data):
         start = int(time.time())
         cmd = sqoop_template % row
+        if row['split_by'].strip():
+            cmd += ' --split-by %s ' % row['split_by']
+        cmd += ('2>&1 | tee sqoop-%(schema)s.%(table)s-%(ts)s-err.log') % {
+                'schema': row['schema'],
+                'table': row['table'], 
+                'ts': datetime.fromtimestamp(start).strftime('%Y-%m-%d-%H-%M-%S')}
         exitcode = os.system(cmd)
         end = int(time.time())
         result = {
@@ -41,10 +50,15 @@ def start(sessionid, sources, nifi_url):
             'CMD': cmd,
             'SCHEMA': row['schema'],
             'TABLE': row['table'],
+            'SPLIT_BY': row['split_by'],
             'MAPPER': row['mapper'],
             'STATUS': 'fail' if exitcode else 'success'
         }
         send_nifi(nifi_url, result)
+        print "%s seconds taken" % (end-start)
+        if idx != source_counti:
+            print "sleeping for 60seconds"
+        time.sleep(60)
 
 
     
