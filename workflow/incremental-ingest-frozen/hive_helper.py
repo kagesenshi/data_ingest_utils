c003944ab6c7aac192195017a7691027dddc98b7
Izhar Firdaus <kagesenshi.87@gmail.com>
2017-02-24 13:05:39 +0800

    guess type of check_column based on output

--- workflow/incremental-ingest-frozen/hive_helper.py
+++ workflow/incremental-ingest-frozen/hive_helper.py
@@ -9,15 +9,62 @@ import shutil
 import tempfile
 import re
 
-SCRIPT='''
+CREATE_SCRIPT='''
 create temporary external table default.%(tablename)s (
     %(table_columns)s
 ) STORED AS PARQUET
 LOCATION "%(path)s";
-
-select %(op)s(%(check_column)s) from default.%(tablename)s;
 '''
 
+def guess_type(sql_args):
+    query = CREATE_SCRIPT + '''
+        select %(check_column)s from default.%(tablename)s where %(check_column)s is
+        not null limit 1;
+    '''
+
+    result = execute_query(query, **sql_args)
+    if re.match('\d+', result):
+        return 'bigint'
+    return None
+
+def get_check_column_value(sql_args, data_type=None):
+    query = CREATE_SCRIPT
+    if data_type is not None:
+        query += '''
+            select %%(op)s(
+                    %(data_type)s(
+                            %%(check_column)s
+                    )
+            ) from default.%%(tablename)s;
+        ''' % { 'data_type': data_type }
+    else:
+        query += '''
+            select %(op)s(%(check_column)s) from default.%(tablename)s;
+        '''
+    result = execute_query(query, **sql_args)
+
+    if re.match(r'\d+-\d+-\d+ \d+:\d+:\d+\.\d+', result):
+       result = (
+           "TO_TIMESTAMP('%s', 'YYYY-MM-DD HH24:MI:SS.FF')" % check_column_value
+       )
+    return result
+
+def execute_query(query, **kwargs):
+    scriptname = tempfile.mktemp()
+    script = '%s.sql' % scriptname
+    tablename = os.path.basename(scriptname)
+    kwargs['tablename'] = 'increment_%s' % (tablename)
+    with open(script, 'w') as s:
+        s.write(query % kwargs)
+    env = os.environ.copy()
+    p = subprocess.Popen(['hive','-f', script],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+    if p.wait() != 0:
+        sys.stderr.write(p.stderr.read())
+        sys.exit(1)
+
+    result = p.stdout.read().strip()
+    return result
 
 def main():
     parser = argparse.ArgumentParser()
@@ -38,29 +85,17 @@ def main():
         sys.stderr.write('Tez Client is not installed')
         sys.exit(1)
 
-    scriptname = tempfile.mktemp()
-    script = '%s.sql' % scriptname
-    tablename = os.path.basename(scriptname)
-    with open(script, 'w') as s:
-        s.write(SCRIPT % {
-             'path': args.path,
-             'table_columns': args.table_columns,
-             'check_column': args.check_column,
-             'op': args.operation,
-             'tablename': 'increment_%s' % (tablename)
-        })
-    env = os.environ.copy()
-    p = subprocess.Popen(['hive','-f', script],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
-    if p.wait() != 0:
-        sys.stderr.write(p.stderr.read())
-        sys.exit(1)
+    params = {
+        'path': args.path,
+        'table_columns': args.table_columns,
+        'check_column': args.check_column,
+        'op': args.operation,
 
-    check_column_value = p.stdout.read().strip()
-    if re.match(r'\d+-\d+-\d+ \d+:\d+:\d+\.\d+', check_column_value):
-       check_column_value = (
-           "TO_TIMESTAMP('%s', 'YYYY-MM-DD HH24:MI:SS.FF')" % check_column_value
-       )
+    }
+
+    data_type = guess_type(params)
+    check_column_value = get_check_column_value(params, data_type=data_type)
+
     out = {
       'CHECK_COLUMN_VALUE': check_column_value,
       'YEAR': now.strftime('%Y'),
